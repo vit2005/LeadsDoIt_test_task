fd9bec3ce4e3e166af9d7adec841b9d32ebe628d
Vit2005 <elstan88@gmail.com>
2024-02-23 20:16:46 +0100

    Added oil puddle

--- Assets/Scripts/GameplayObjects/Implementation/SlowGameplayObject.cs
+++ Assets/Scripts/GameplayObjects/Implementation/SlowGameplayObject.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class SlowGameplayObject : GameplayObject
 {
@@ -12,4 +13,11 @@ public class SlowGameplayObject : GameplayObject
     public override ObjectType objectType => ObjectType.Slow;
 
     public override BuffId? AutoApplyBuffId => BuffId.Slow;
+
+    public override void Init()
+    {
+        base.Init();
+        transform.Rotate(0f, 0f, UnityEngine.Random.Range(0f, 360f));
+        transform.localScale = Vector3.one * UnityEngine.Random.Range(0.15f, 0.18f);
+    }
 }
